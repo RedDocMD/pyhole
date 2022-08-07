daf66b2c0e460d6c748b1b923886a715e5622306
Deep Majumder <deep.majumder2019@gmail.com>
2022-08-08 00:11:22 +0530

    Creating AST visitor, currently visits modules

--- pyhole/object.py
+++ pyhole/object.py
@@ -1,4 +1,6 @@
 from pathlib import PurePath
+from typing import Any, Union
+import ast
 
 
 class SourceSpan:
@@ -29,13 +31,15 @@ class ObjectPath:
 
 class Object:
     source_span: SourceSpan
-    parent: 'Object'
+    parent: Union['Object', None]
+    children: list['Object']
     name: str
 
     def __init__(self, source_span: SourceSpan, name: str, parent: 'Object' = None) -> None:
         self.source_span = source_span
         self.parent = parent
         self.name = name
+        self.children = []
 
     def full_path(self) -> ObjectPath:
         path = ObjectPath()
@@ -45,9 +49,13 @@ class Object:
             obj = obj.parent
         return path
 
+    def append_child(self, child: 'Object') -> None:
+        self.children.append(child)
+
 
 class Module(Object):
-    pass
+    def __init__(self, source_span: SourceSpan, name: str, parent: 'Object' = None) -> None:
+        super().__init__(source_span, name, parent)
 
 
 class Class(Object):
@@ -56,3 +64,38 @@ class Class(Object):
 
 class Function(Object):
     pass
+
+
+class ObjectCreator(ast.NodeVisitor):
+    ob_stack: list[Object]
+    filename: PurePath
+
+    def __init__(self, filename: PurePath):
+        self.ob_stack = []
+        self.filename = filename
+
+    def _parent(self) -> Union[Object, None]:
+        if len(self.ob_stack) == 0:
+            return self.ob_stack[-1]
+        else:
+            return None
+
+    def _source_span(self, node: ast.AST) -> SourceSpan:
+        return SourceSpan(self.filename, node.lineno, node.end_lineno)
+
+    def _mod_name(self) -> str:
+        parts = self.filename.parts
+        if parts[-1] == "__init__.py":
+            return parts[-2]
+        else:
+            return parts[-1]
+
+    def visit_Module(self, mod: ast.Module) -> Any:
+        par = self._parent()
+        ss = self._source_span(mod)
+        name = self._mod_name()
+        ob = Module(ss, name, par)
+        # Now visit children
+        self.ob_stack.append(ob)
+        self.generic_visit(mod)
+        self.ob_stack.pop()
